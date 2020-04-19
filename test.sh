f51ed89dd2fc05c2b2dff87f71f468f7b2064490
Carl Hetherington <cth@carlh.net>
2020-04-19 23:13:11 +0200

    Update test runner.

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 file=$HOME/DCP/betty_stereo.wav
-LD_LIBRARY_PATH=build/src build/src/leqm-nrt $file -chconfcal 0 0 > test
+LD_LIBRARY_PATH=build/src build/src/leqm_nrt $file -chconfcal 0 0 > test
 diff -u ref test && echo "PASS" || echo "FAIL"
